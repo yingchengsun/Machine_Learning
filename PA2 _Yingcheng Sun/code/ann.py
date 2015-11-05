1990e467ea3333e74a8b0c423f42b047f8d63627
Yingcheng(Jason) Sun <yxs489@case.edu>
2015-11-04 23:20:58 -0500

    ANN

---
+++ PA2 _Yingcheng Sun/code/ann.py
@@ -0,0 +1,133 @@
+"""
+The Artificial Neural Network
+"""
+import numpy as np
+import scipy
+
+class ArtificialNeuralNetwork(object):
+
+
+    def __init__(self, gamma, layer_sizes, num_hidden, epsilon=None, max_iters=None):
+        """
+        Construct an artificial neural network classifier
+
+        @param gamma : weight decay coefficient
+        @param layer_sizes:  Number of hidden layers
+        @param num_hidden:  Number of hidden units in each hidden layer
+        @param epsilon : cutoff for gradient descent
+                         (need at least one of [epsilon, max_iters])
+        @param max_iters : maximum number of iterations to run
+                            gradient descent for
+                            (need at least one of [epsilon, max_iters])
+        """
+        self.gamma=gamma
+        self.layer_sizes=layer_sizes
+        self.num_hidden=num_hidden
+        self.epsilon=epsilon
+        self.max_iters=max_iters
+        self.syn0=np.array([])
+        self.syn1=np.array([])
+        self.learning_rate=0.01
+
+        
+    def fit(self, X, y, schema, sample_weight=None):
+        """ Fit a neural network of layer_sizes * num_hidden hidden units using X, y """
+        X_array=np.asarray(X) 
+
+        X_data_int=X_array[:,0:X_array.shape[1]-1]
+        y_data=X_array[:,X_array.shape[1]-1:X_array.shape[1]]
+        X_data= X_data_int/1.0
+        #max_iterations=len(X_data)*100
+        #max_iterations=100
+        
+        """Data Standardization"""
+        for i in range(len(X_data[0])):
+            if schema.is_nominal(i):
+                nominal_mapping=np.array(schema.nominal_values[i])
+                for m in range(len(X_data[:,i])):
+                    for n in range(len(nominal_mapping)):
+                        if np.float64((nominal_mapping[n]))==X_data[:,i][m]:
+                            X_data[:,i][m]=n                         
+            else:
+                X_data[:,i]= (X_data[:,i]-min(X_data[:,i]))/float(max(X_data[:,i])-min(X_data[:,i])) 
+                           
+        for i in range(len(y_data)):
+            if y_data[i]==-1:
+                y_data[i]=y_data[i]+1
+                
+        np.random.seed(1)
+        
+        # randomly initialize our weights with mean 0 from (-1,1)
+        self.syn0 = (2*np.random.random((X_data.shape[1],self.num_hidden)) - 1)/10.0
+        self.syn1 = (2*np.random.random((self.num_hidden,1)) - 1)/10.0
+        #self.syn0 = (2*np.random.random((X_data.shape[1],1)) - 1)/10.0
+        iteration = 0
+        while True:            # Feed forward through layers 0, 1, and 2
+            l0 = X_data
+            l1 = 1/(1+np.exp(-np.dot(l0,self.syn0)))
+            l2 = 1/(1+np.exp(-np.dot(l1,self.syn1)))
+
+            #l1_delta=l0.T.dot((np.dot(l0,self.syn0)-y_data))
+            #self.syn0+=-l1_delta*self.learning_rate
+            
+            # how much did we miss the target value?
+            l2_error = y_data - l2
+                       
+            # in what direction is the target value?
+            # were we really sure? if so, don't change too much.
+            l2_delta = l2_error*(l2*(1-l2))
+            
+            # how much did each l1 value contribute to the l2 error (according to the weights)?
+            l1_error = l2_delta.dot(self.syn1.T)
+            
+            # in what direction is the target l1?
+            # were we really sure? if so, don't change too much.
+            l1_delta = l1_error *(l1*(1-l1))
+            
+            #self.syn1 =self.syn1+ (l1.T.dot(l2_delta))*self.learning_rate
+            #self.syn0 =self.syn0+ (l0.T.dot(l1_delta))*self.learning_rate
+            
+            self.syn1 += (l1.T.dot(l2_delta))*self.learning_rate-self.gamma*self.learning_rate*self.syn1
+            self.syn0 += (l0.T.dot(l1_delta))*self.learning_rate-self.gamma*self.learning_rate*self.syn0
+            iteration += 1 
+            '''
+            if max_iterations!=0 and iteration >= max_iterations: 
+                break
+            elif max_iterations==0 and np.mean(np.abs(l1_delta)) < 1e-3 and np.mean(np.abs(l2_delta)) < 1e-3:
+                break
+            '''
+            if self.max_iters!=0 and iteration >= self.max_iters: 
+                break
+            elif self.max_iters==0 and np.mean(np.abs(l2_error)) < 1e-3 and np.mean(np.abs(l2_delta)) < 1e-3:
+                break
+                
+    def predict(self, X):
+        """ Predict -1/1 output """
+        X_array=np.asarray(X) 
+        X_test=X_array[:,0:X_array.shape[1]-1]
+        l0_test = X_test
+        l1_test = 1/(1+np.exp(-np.dot(l0_test,self.syn0)))
+        l2_test = 1/(1+np.exp(-np.dot(l1_test,self.syn1)))
+        #predictions=np.ones((len(l2_test),1))
+        
+        for j in range(len(l2_test)):
+            if l2_test[j]>0.5:
+                l2_test[j]=1.0
+            else:
+                l2_test[j]=-1.0
+                
+        return l2_test
+
+    def predict_proba(self, X):
+        """ Predict probabilistic output """
+        X_array=np.asarray(X) 
+        X_test=X_array[:,0:X_array.shape[1]-1]
+        l0_test = X_test
+        l1_test = 1/(1+np.exp(-np.dot(l0_test,self.syn0)))
+        l2_test = 1/(1+np.exp(-np.dot(l1_test,self.syn1)))
+        l2_test_no_column=l2_test.reshape((len(l2_test),))
+        return l2_test_no_column
+    
+    
+    
+    
---
+++ PA2 _Yingcheng Sun/code/stats.py
@@ -0,0 +1,126 @@
+"""
+Statistics Computations
+"""
+import numpy as np
+import scipy
+
+
+class StatisticsManager(object):
+
+    def __init__(self):
+        self.true_labels = []
+        self.predicted_labels = []
+        self.prediction_scores = []
+        self.training_times = []
+        self.statistics = {
+            'accuracy' : (accuracy,  self.predicted_labels),
+            'precision': (precision, self.predicted_labels),
+            'recall'   : (recall,    self.predicted_labels),
+            'auc'      : (auc,       self.prediction_scores),
+        }
+
+    def add_fold(self, true_labels, predicted_labels,
+                 prediction_scores, training_time):
+        """
+        Add a fold of labels and predictions for later statistics computations
+
+        @param true_labels : the actual labels
+        @param predicted_labels : the predicted binary labels
+        @param prediction_scores : the real-valued confidence values
+        @param training_time : how long it took to train on the fold
+        """
+        self.true_labels.append(true_labels)
+        self.predicted_labels.append(predicted_labels)
+        self.prediction_scores.append(prediction_scores)
+        self.training_times.append(training_time)
+
+    def get_statistic(self, statistic_name, pooled=True):
+        """
+        Get a statistic by name, either pooled across folds or not
+
+        @param statistic_name : one of {accuracy, precision, recall, auc}
+        @param pooled=True : whether or not to "pool" predictions across folds
+        @return statistic if pooled, or (avg, std) of statistic across folds
+        """
+        
+        if statistic_name not in self.statistics:
+            raise ValueError('"%s" not implemented' % statistic_name)
+
+        statistic, predictions = self.statistics[statistic_name]
+
+        if pooled:
+            predictions = np.hstack(map(np.asarray, predictions))
+            labels = np.hstack(map(np.asarray, self.true_labels))
+            return statistic(labels, predictions)
+        else:
+            stats = []
+            for l, p in zip(self.true_labels, predictions):
+                stats.append(statistic(l, p))
+            return np.average(stats), np.std(stats)
+
+def accuracy(labels, predictions):
+    count=0.0
+    for i in range(len(labels)):
+        if labels[i]==predictions[i]:
+            count+=1
+    return count/len(labels)
+
+def precision(labels, predictions):
+    TP=0.0
+    FP=0.0
+    for i in range(len(labels)):
+        if (labels[i]==1) and (predictions[i]==1):
+            TP+=1
+        if (labels[i]!=1) and (predictions[i]==1):
+            FP+=1
+    if TP+FP==0:
+        return 0
+    else:
+        return TP/(TP+FP)
+   
+def recall(labels, predictions):
+    TP=0.0
+    FN=0.0
+    for i in range(len(labels)):
+        if (labels[i]==1) and (predictions[i]==1):
+            TP+=1
+        if (labels[i]!=-1) and (predictions[i]==-1):
+            FN+=1
+    return TP/(TP+FN)
+
+def auc(labels, predictions):
+    TP_rate = []
+    FP_rate = []
+    #thresh=0.5
+    
+    #need to sort the data by result
+    results,labels = zip(*sorted(zip(predictions,labels),reverse=True))
+    #calculation for AROC
+    for thresh in results:
+        TP=FP=FN=TN=0.0
+        for i in range(len(labels)):
+            if results[i]>thresh and labels[i]==1:
+                TP+=1
+            if results[i]>thresh and labels[i]==-1:
+                FP+=1
+            if results[i]<=thresh and labels[i]==1:
+                FN+=1
+            if results[i]<=thresh and labels[i]==-1:
+                TN+=1
+        if (FP+TN)!=0:
+            FP_rate.append(FP/float(FP+TN))
+        else:
+            FP_rate.append(0.0)
+        if (TP+FN)!=0:
+            TP_rate.append(TP/float(TP+FN))
+        else:
+            TP_rate.append(0.0)
+        
+    #get the last one
+    #tp,fn,fp,tn = cont_table(ROC_set,results,results[-1]*.9)
+    aroc = 0  
+    for p1,p2 in zip(zip(FP_rate[0:-1],TP_rate[0:-1]),zip(FP_rate[1:],TP_rate[1:])):
+        aroc += (p2[0]-p1[0])*(p2[1]+p1[1])/2.0  
+        
+    return aroc
+    
